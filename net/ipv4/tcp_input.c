1af1b557891a8b4921fdab6fc48249608da7f491
Olivier Karasangabo <olivier@lineageos.org>
2017-05-21 17:59:29 +0200

    Linux v3.10.105
    Change-Id: I8b16ebed138451e4640d725571905fe72eb715e0

--- net/ipv4/tcp_input.c
+++ net/ipv4/tcp_input.c
@@ -3296,13 +3296,13 @@ static void tcp_send_challenge_ack(struct sock *sk)
 		u32 half = (sysctl_tcp_challenge_ack_limit + 1) >> 1;
 
 		challenge_timestamp = now;
-		ACCESS_ONCE(challenge_count) =  half +
-			   reciprocal_divide(prandom_u32(),
-				sysctl_tcp_challenge_ack_limit);
+		ACCESS_ONCE(challenge_count) = half +
+				  reciprocal_divide(prandom_u32(),
+					sysctl_tcp_challenge_ack_limit);
 	}
 	count = ACCESS_ONCE(challenge_count);
 	if (count > 0) {
-		ACCESS_ONCE(challenge_count) =  count - 1;
+		ACCESS_ONCE(challenge_count) = count - 1;
 		NET_INC_STATS_BH(sock_net(sk), LINUX_MIB_TCPCHALLENGEACK);
 		tcp_send_ack(sk);
 	}
@@ -5586,6 +5586,7 @@ discard:
 		}
 
 		tp->rcv_nxt = TCP_SKB_CB(skb)->seq + 1;
+		tp->copied_seq = tp->rcv_nxt;
 		tp->rcv_wup = TCP_SKB_CB(skb)->seq + 1;
 
 		/* RFC1323: The window in SYN & SYN/ACK segments is
