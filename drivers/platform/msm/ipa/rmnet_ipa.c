d23f4699cdc95d5fd305e5513ff23f97d2c26c7f
Skylar Chang <chiaweic@codeaurora.org>
2017-08-11 15:49:21 -0700

    msm: ipa3: add lock for num_q6_rule
    There is a race condition be observed
    on global variable num_q6_rule used in
    ipa wan-driver. The fix is to add lock
    to prevent different threads are accessing
    it at the same time.
    
    Change-Id: Ia9190c60361cb5605b61963309beca3acdeac89d
    Acked-by: Pooja Kumari <kumarip@qti.qualcomm.com>
    Signed-off-by: Mohammed Javid <mjavid@codeaurora.org>
    Signed-off-by: Skylar Chang <chiaweic@codeaurora.org>
    (cherry picked from commit 3091455147aa37733b8b325665eecb90649e6911)

--- drivers/platform/msm/ipa/rmnet_ipa.c
+++ drivers/platform/msm/ipa/rmnet_ipa.c
@@ -385,12 +385,15 @@ int copy_ul_filter_rule_to_ipa(struct ipa_install_fltr_rule_req_msg_v01
 {
 	int rc = 0, i, j;
 
+	/* prevent multi-threads accessing num_q6_rule */
+	mutex_lock(&add_mux_channel_lock);
 	if (rule_req->filter_spec_list_valid == true) {
 		num_q6_rule = rule_req->filter_spec_list_len;
 		IPAWANDBG("Received (%d) install_flt_req\n", num_q6_rule);
 	} else {
 		num_q6_rule = 0;
 		IPAWANERR("got no UL rules from modem\n");
+		mutex_unlock(&add_mux_channel_lock);
 		return -EINVAL;
 	}
 	/* copy UL filter rules from Modem*/
@@ -549,6 +552,7 @@ int copy_ul_filter_rule_to_ipa(struct ipa_install_fltr_rule_req_msg_v01
 			ipv4_frag_eq_present = rule_req->filter_spec_list[i].
 			filter_rule.ipv4_frag_eq_present;
 	}
+	mutex_unlock(&add_mux_channel_lock);
 	return rc;
 }
 
@@ -1352,6 +1356,8 @@ static int ipa_wwan_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 				IPAWANERR("failed to config egress endpoint\n");
 
 			if (num_q6_rule != 0) {
+				/* protect num_q6_rule */
+				mutex_lock(&add_mux_channel_lock);
 				/* already got Q6 UL filter rules*/
 				rc = wwan_add_ul_flt_rule_to_ipa();
 				egress_set = true;
@@ -1359,6 +1365,7 @@ static int ipa_wwan_ioctl(struct net_device *dev, struct ifreq *ifr, int cmd)
 					IPAWANERR("install UL rules failed\n");
 				else
 					a7_ul_flt_set = true;
+				mutex_unlock(&add_mux_channel_lock);
 			} else {
 				/* wait Q6 UL filter rules*/
 				egress_set = true;
@@ -2611,6 +2618,8 @@ static int __init ipa_wwan_init(void)
 
 	atomic_set(&is_initialized, 0);
 	atomic_set(&is_ssr, 0);
+
+	mutex_init(&add_mux_channel_lock);
 
 	ipa_qmi_init();
 
