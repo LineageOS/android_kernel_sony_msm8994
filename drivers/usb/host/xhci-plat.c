b3508c982b3ca42e1b717fd15fc620881ff8a69b
threader <michael.heltne@gmail.com>
2020-05-14 17:28:25 +0100

    drivers: usb: could not find the commit for these

--- drivers/usb/host/xhci-plat.c
+++ drivers/usb/host/xhci-plat.c
@@ -207,6 +207,8 @@ static int xhci_plat_remove(struct platform_device *dev)
 
 	xhci->xhc_state |= XHCI_STATE_REMOVING;
 
+	pm_runtime_disable(&dev->dev);
+
 	usb_remove_hcd(xhci->shared_hcd);
 	usb_put_hcd(xhci->shared_hcd);
 
